71b65a2ac5771fa93e6d2b40aa4caa8c74b3f0cf
GuilhermeRamalho52 <guilherme.ramalho03@gmail.com>
2022-12-07 12:26:11 +0000

    final menu

--- Projeto P1/Funcao_Menu/main.c
+++ Projeto P1/Funcao_Menu/main.c
@@ -2,31 +2,12 @@
 #include <stdlib.h>
 #include <locale.h>
 
+
 char menu()
 {
-    setlocale(LC_ALL,"");
+    setlocale(LC_ALL, "");
     char op;
     printf("\t\t\t\tMenu Principal\n\n");
-    printf("Portateis existentes: %d\t\t\t\t\tNumero Total de requisicoes efetuadas: %d");
-    printf("\nPortateis disponiveis: %d\t\t\t\t\tNumero de requisicoes ativas: %d");
-    printf("\n\n[A]Adicionar Portatil");
-    printf("\n[B]Listar Portateis");
-    printf("\n[C]Alterar localização do Portátil.");
-    printf("\n[D]Devolução do Portátil");
-    printf("\n[E]Renovar Requisição");
-    printf("\n[F]Registar Avaria");
-    printf("\n[G]Estado do Portátil");
-    printf("\n[H]Média de multa");
-    printf("\n[I]Média de cada processador utilizado");
-    printf("\n[J]Tipos de utentes com menor quantidade de requisições");
-    printf("\n[K]Devoluções mais Recentes");
-    printf("\n[L]Todas as avarias");
-    printf("\nOpcao >> ");
-    scanf(" %c", &op);
-    return op;
-}
-
-/*printf("\t\t\t\tMenu Principal\n\n");
     printf("Portateis existentes: \t\t\t\t\tNumero Total de requisicoes efetuadas: ");
     printf("\nPortateis disponiveis: \t\t\t\t\tNumero de requisicoes ativas: ");
     printf("\n\n[A]Adicionar Portatil");
@@ -45,7 +26,14 @@ char menu()
     printf("\n[N]Dados Estatisticos");
     printf("\n[O] SAIR");
     printf("\nOpcao >> ");
-*/
+    scanf(" %c", &op);
+    op = toupper(op);
+    return op;
+}
+
+
+
+
 
 int main(){
     menu();
