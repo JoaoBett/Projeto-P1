7e57a8a870c4fdb00a3ea7594d573ae1d17715ef
JoaoBett <joao.gouveia.bettencourt@gmail.com>
2023-02-03 02:14:57 +0000

    exclude: duplicated project

--- ProjetoP1_Ramalho_Bettencourt_2/ficheiros.h
+++
@@ -1,7 +0,0 @@
-#ifndef FICHEIROS_H
-#define FICHEIROS_H
-
-void gravarFicheiroBinario(tipoPc portateis[MAX_PORTATIL], int quantidadePortateis, tipoRequisicao* requesicao, int quantidadeRequesicoes);
-int leFicheiroBinario(tipoPc vetorPc[]);
-#endif // FICHEIROS_H
-
